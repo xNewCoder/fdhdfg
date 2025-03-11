8cd38d54d71f2e3afdf44d69599f559a67517784
xNewCoder <132601971+xNewCoder@users.noreply.github.com>
2025-03-11 10:27:29 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -337,7 +337,7 @@ const setDataFromConfigToHtml = async () => {
     } else if(locationPathname.includes("rules")) {
         copyIp();
     }
-    else if(locationPathname.includes("admin-team")) {
+    else if(locationPathname.includes("adminteam")) {
         for (let team in config.adminTeamPage) {
             const atContent = document.querySelector(".at-content");
             
